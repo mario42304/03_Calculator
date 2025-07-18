0aa776ccf03b8b61a6d6fd5b7d8192652d12f6f7
mario42304 <gotohda42@gmail.com>
2025-07-18 16:45:24 +0900

    build0002

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,76 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [answer, setAnswer] = useState('')
+  const [inputValue, setInputValue] = useState('')
+  const [operator, setOperator] = useState<string | null>(null)
+  const [overWrite, setOverwrite] = useState(false)
+
+  const handleInput = (input: string) => {
+    setInputValue(prev => {
+      if (prev.length >= 20) {
+        return prev
+      }
+      return prev + input
+    })
+  }
+
+  const handleBackspace = () => {
+    setInputValue(prev => prev.slice(0, -1))
+  }
+
+  const handleSignFlip = () => {
+    setInputValue(prev => {
+      if(prev.endsWith('±')){
+        return prev.slice(0, -1)
+      }
+      return prev + '±'
+    })
+  }
+
+  const handleCaluculate = () => {
+
+  }
+
+  const handleInputError = () => {
+    throw new Error("SYNTAX ERROR");
+  }
 
   return (
     <>
+      <p>{inputValue.replace(/±/g, '-')}</p>
+      <p>{answer}</p>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={() => handleInput('7')}>7</button>
+        <button onClick={() => handleInput('8')}>8</button>
+        <button onClick={() => handleInput('9')}>9</button>
+        <button onClick={() => handleInput('+')}>+</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <button onClick={() => handleInput('4')}>4</button>
+        <button onClick={() => handleInput('5')}>5</button>
+        <button onClick={() => handleInput('6')}>6</button>
+        <button onClick={() => handleInput('-')}>-</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <button onClick={() => handleInput('1')}>1</button>
+        <button onClick={() => handleInput('2')}>2</button>
+        <button onClick={() => handleInput('3')}>3</button>
+        <button onClick={() => handleInput('*')}>*</button>
+      </div>
+      <div>
+        <button onClick={() => handleSignFlip()}>±</button>
+        <button onClick={() => handleInput('0')}>0</button>
+        <button onClick={() => handleInput('.')}>.</button>
+        <button onClick={() => handleInput('/')}>/</button>
+      </div>
+      <div>
+        <button onClick={() => handleCaluculate()}>=</button>
+        <button onClick={() => handleBackspace()}>BS</button>
+        <button onClick={() => handleInput('')}></button>
+        <button onClick={() => handleInput('')}></button>
+      </div>
     </>
   )
 }
